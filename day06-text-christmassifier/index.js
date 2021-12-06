3d097bdf9faf019ec694ee97e7735a147234f126
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-05 19:49:57 -0500

    Build out function logic

--- day06-text-christmassifier/index.js
+++ day06-text-christmassifier/index.js
@@ -5,7 +5,16 @@ christmassifierBtn.addEventListener('click', christmassifyName)
 function christmassifyName() {
   // Task:
   // - Add christmassify class to greeting.
-  //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
+  // - Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
+  if (christmassifierBtn.innerText.toLowerCase() === 'christmassify') {
+    greeting.classList.add('christmassified')
+    christmassifierBtn.innerText = 'De-christmassify'
+  } else if (
+    christmassifierBtn.innerText.toLowerCase() === 'de-christmassify'
+  ) {
+    greeting.classList.remove('christmassified')
+    christmassifierBtn.innerText = 'Christmassify'
+  }
 }
 
 // Stretch goals:
