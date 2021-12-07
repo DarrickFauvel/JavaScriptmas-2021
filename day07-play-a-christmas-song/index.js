5b8be3e8ce0744bfb024796e63d6a64707ed4829
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-07 10:01:09 -0500

    Add working audio controls feature

--- day07-play-a-christmas-song/index.js
+++ day07-play-a-christmas-song/index.js
@@ -4,7 +4,14 @@ const stopBtn = document.getElementById('stop-btn')
 
 // Task:
 // - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
+const audioEl = new Audio('bells.mp3')
 
+playBtn.addEventListener('click', () => audioEl.play())
+pauseBtn.addEventListener('click', () => audioEl.pause())
+stopBtn.addEventListener('click', () => {
+  audioEl.pause()
+  audioEl.currentTime = 0
+})
 // Stretch goals:
 // - Add volume controls.
 // - Allow the user to select different sounds.
