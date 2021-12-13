be843da6e8da018cb3e3a3fbebdb1c53f5ad1b17
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-13 08:53:43 -0500

    Create logic for challenge solution

--- day13-christmas-dinner-calculator/index.js
+++ day13-christmas-dinner-calculator/index.js
@@ -1,13 +1,41 @@
 const btn = document.getElementById('btn')
-let food = document.getElementById('food')
+const food = document.getElementById('food')
+const guestsInput = document.getElementById('guests')
+const vegetarianCheckbox = document.getElementById('vegetarian')
+const formEl = document.querySelector('form')
+
+let isVegetarian = false
+let dinnerSuggestion = ''
 
 // Tasks:
-// - Write the JS to decide the perfect Christmas dinner and render it in the result element. Don't forget to check whether the meal should be vegetarian!
+// - Write the JS to decide the perfect Christmas dinner and render it in the result element.
+//   Don't forget to check whether the meal should be vegetarian!
 
 // Dinner suggestions (or choose your own!):
 // Vegetarian: nut roast
 // 4 people or less: turkey
 // 5+ people: goose
+const checkNumberOfGuests = () => {
+  dinnerSuggestion = guestsInput.value < 5 ? 'turkey' : 'goose'
+}
+
+const checkIsVegetarian = () => {
+  isVegetarian = vegetarianCheckbox.checked ? true : false
+}
+
+const makeDinnerSuggestion = () => {
+  dinnerSuggestion = isVegetarian ? 'nut roast' : dinnerSuggestion
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  checkNumberOfGuests()
+  checkIsVegetarian()
+  makeDinnerSuggestion()
+  food.textContent = dinnerSuggestion
+}
+
+btn.addEventListener('click', handleSubmit)
 
 // Stretch goals:
 // - Add more dietary options.
