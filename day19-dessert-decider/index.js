910544245f60202b61b0962ba8d2171b95e1c751
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-18 21:13:04 -0500

    Create Day 19 solution

--- day19-dessert-decider/index.js
+++ day19-dessert-decider/index.js
@@ -1,13 +1,27 @@
+const foodHolder = document.getElementById('foodHolder')
 const btn = document.getElementById('btn')
-// btn.addEventListener("click", findYum)
+const API_URL = 'https://foodish-api.herokuapp.com/api/images/dessert'
 
 /* Task:
 Call the Foodish API (https://foodish-api.herokuapp.com/) and display random images of desserts on the click of a button.
 
 Specific URL to get a random dessert image: 
-https://foodish-api.herokuapp.com/api/images/dessert
+https://foodish-api.herokuapp.com/api/images/dessert */
 
-Stretch goals: 
+const findYum = async () => {
+  const response = await fetch(API_URL)
+  const data = await response.json()
+
+  foodHolder.innerHTML = ''
+  const imgEl = document.createElement('img')
+  imgEl.setAttribute('src', data.image)
+  imgEl.setAttribute('alt', 'dessert photo')
+  foodHolder.appendChild(imgEl)
+}
+
+btn.addEventListener('click', findYum)
+
+/* Stretch goals: 
 - Show multiple desserts.
 - Add the functionality to go back to the previous image.
 */
