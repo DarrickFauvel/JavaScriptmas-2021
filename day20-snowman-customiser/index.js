856e841d082fdf10a25a561e94b1e6f8efda83c1
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-19 20:23:20 -0500

    Solve Day 20 challenge

--- day20-snowman-customiser/index.js
+++ day20-snowman-customiser/index.js
@@ -2,6 +2,20 @@ const inputs = document.querySelectorAll('.controls input')
 
 // Task:
 // Write a function to update the snowman colors according to the colors selected from the pickers.
+const handleChange = (e) => {
+  const bodyPartName = e.target.dataset.partName
+  const selectedColorValue = e.target.value
+  const bodyPartElements = document.querySelectorAll(`.${bodyPartName}`)
+  bodyPartElements.forEach((part) => {
+    if (bodyPartName === 'nose') {
+      part.style.borderColor = `transparent transparent transparent ${selectedColorValue}`
+    } else {
+      part.style.backgroundColor = selectedColorValue
+    }
+  })
+}
+
+inputs.forEach((input) => input.addEventListener('change', handleChange))
 
 // Stretch goals:
 // - Add other items eg scarf, arms, etc.
