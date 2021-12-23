f64fe0f3a6b83e48b6acad85be2b1a0a4dba4eda
Darrick Fauvel <darrick.fauvel@gmail.com>
2021-12-23 16:15:33 -0500

    Solve Day 23 challenge

--- day23-customisable-santa-sign/index.js
+++ day23-customisable-santa-sign/index.js
@@ -1,5 +1,13 @@
+const sign = document.getElementById('sign')
+const textInput = document.getElementById('text-input')
+
 // Task:
 // - Write a function to update the Santa Stop Here sign with the user's own text.
+const handleChange = (e) => {
+  sign.textContent = e.target.value
+}
+
+textInput.addEventListener('input', handleChange)
 
 // Stretch goals
 // - Add a min and max length to the message.
